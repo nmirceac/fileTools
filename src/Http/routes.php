870f90043351be88dda5dd841705100b11fb0317
DevBox <dev@weanswer.it>
2019-07-17 19:57:48 +0200

    route fix

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,7 +1,7 @@
 <?php
 
 $router->group(['middleware' => config('filetools.router.authMiddleware')], function ($router) {
-    $router::post('/files/{any}', ['uses' => 'FilesController@vueHelper', 'as' => config('filetools.router.namedPrefix').'.helper']);
-    $router::get('/files/preview/{fileId}', ['uses' => 'FilesController@preview', 'as' => config('filetools.router.namedPrefix').'.preview']);
-    $router::get('/files/download/{fileId}', ['uses' => 'FilesController@download', 'as' => config('filetools.router.namedPrefix').'.download']);
+    $router->post('/files/{any}', ['uses' => 'FilesController@vueHelper', 'as' => config('filetools.router.namedPrefix').'.helper']);
+    $router->get('/files/preview/{fileId}', ['uses' => 'FilesController@preview', 'as' => config('filetools.router.namedPrefix').'.preview']);
+    $router->get('/files/download/{fileId}', ['uses' => 'FilesController@download', 'as' => config('filetools.router.namedPrefix').'.download']);
 });
