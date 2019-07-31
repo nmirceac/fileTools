d2f64b8a5586e7fdd987245e0a67a98aca0ffe97
DevBox <dev@weanswer.it>
2019-07-31 09:22:22 +0200

    s3 file controller optimizatio

--- src/Http/Controllers/FilesController.php
+++ src/Http/Controllers/FilesController.php
@@ -4,11 +4,17 @@ class FilesController extends \App\Http\Controllers\Controller
 {
     public function preview($id)
     {
+        if(strtolower(config('filetools.storage.backend')) == 's3') {
+            return redirect(\App\File::find($id)->getUrl());
+        }
         return \App\File::find($id)->serve();
     }
 
     public function download($id)
     {
+        if(strtolower(config('filetools.storage.backend')) == 's3') {
+            return redirect(\App\File::find($id)->downloadUrl());
+        }
         return \App\File::find($id)->serveForceDownload();
     }
 }
