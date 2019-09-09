265f69faf7023a472e85b8e98a3cfdcf1d1a7e25
DevBox <dev@weanswer.it>
2019-09-09 15:55:08 +0200

    files tools controller

--- src/Http/Controllers/FilesController.php
+++ src/Http/Controllers/FilesController.php
@@ -17,5 +17,128 @@ class FilesController extends \App\Http\Controllers\Controller
         }
         return \App\File::find($id)->serveForceDownload();
     }
+
+    public function upload()
+    {
+        $roleName = request('role');
+
+        $excludeFromDetails = ['file', 'modelId', 'model', 'single', 'role', $roleName];
+        $details = request()->except($excludeFromDetails);
+
+        $file = \App\File::createFromRequest(request(), $roleName);
+
+        if (request('model', false) and request('modelId', false)) {
+            $model = '\\App\\' . ucfirst(request('model'));
+            $model = new $model;
+            $object = $model->find(request('modelId'));
+
+            if (request('single', false)) {
+                $file = $file->set($object, $roleName, $details);
+            } else {
+                $file = $file->attach($object, $roleName, 'last', $details);
+            }
+        }
+
+        return response()->json([$this->itemName => $file]);
+    }
+
+    public function attach()
+    {
+        $file = \App\File::findOrFail(request('id'));
+
+        $roleName = request('role');
+
+        if (request('model', false) and request('modelId', false)) {
+
+            $model = '\\App\\' . ucfirst(request('model'));
+            $model = new $model;
+            $object = $model->find(request('modelId'));
+
+            if (request('single', false)) {
+                $file = $file->set($object, $roleName, []);
+            } else {
+                $file = $file->attach($object, $roleName, 'last', []);
+            }
+        }
+
+        return response()->json([$this->itemName => $file]);
+    }
+
+    public function delete()
+    {
+        $file = \App\File::findOrFail(request('id'));
+
+        if (request('model', false) and request('modelId', false)) {
+
+            $model = '\\App\\' . ucfirst(request('model'));
+            $model = new $model;
+            $object = $model->find(request('modelId'));
+
+            if (request('id')) {
+                $object->clearFile(request('id'));
+            } elseif (request('role')) {
+                $object->clearFilesByRole(request('role'));
+            }
+        }
+
+        $file->delete();
+    }
+
+    public function replace()
+    {
+        $originalFile = \App\File::findOrFail(request('id'));
+        $roleName = request('role');
+
+        $newFile = $originalFile->replaceFromRequest(request(), $roleName);
+
+        $fileMeta = (array)$newFile->metadata;
+
+        $metadata = request()->only(['caption', 'copyright', 'tags']);
+        foreach ($metadata as $key => $data) {
+            if ($data == "null" || is_null($data)) {
+                if ($key !== 'tags') {
+                    $fileMeta[$key] = '';
+                } else {
+                    $fileMeta[$key] = [];
+                }
+            } elseif ($key == 'tags') {
+                $fileMeta['tags'] = explode(',', $data);
+            } else {
+                $fileMeta[$key] = $data;
+            }
+        }
+
+        $newFile->metadata = $fileMeta;
+        $newFile->update();
+
+        return response()->json([$this->itemName => $newFile]);
+    }
+
+    public function update()
+    {
+        $file = \App\File::findOrFail(request('id'));
+
+        if (request('model', false) and request('modelId', false)) {
+            $model = '\\App\\' . ucfirst(request('model'));
+            $model = new $model;
+            $object = $model->find(request('modelId'));
+            $file->attach($object, request('role'), request('order'), request('metadata'));
+        } else {
+            $file->metadata = request('metadata');
+            $file->update();
+        }
+
+        $file->log(Log::LOG_FILE_UPDATED);
+
+        return response()->json([$this->itemName => $file]);
+    }
+
+    public function reorder()
+    {
+        $model = '\\App\\' . ucfirst(request('model'));
+        $model = new $model;
+        $object = $model->find(request('modelId'));
+        $object->reorderFilesByRole(request('ids'), request('role'));
+    }
 }
 
--- src/Http/routes.php
+++ src/Http/routes.php
@@ -3,4 +3,10 @@
 $router->group(['middleware' => config('filetools.router.authMiddleware')], function ($router) {
     $router->get('/preview/{fileId}', ['uses' => 'FilesController@preview', 'as' => config('filetools.router.namedPrefix').'.preview']);
     $router->get('/download/{fileId}', ['uses' => 'FilesController@download', 'as' => config('filetools.router.namedPrefix').'.download']);
+    $router->post('/upload', ['uses' => 'FilesController@upload', 'as' => config('filetools.router.namedPrefix').'.upload']);
+    $router->post('/attach', ['uses' => 'FilesController@attach', 'as' => config('filetools.router.namedPrefix').'.attach']);
+    $router->post('/delete', ['uses' => 'FilesController@delete', 'as' => config('filetools.router.namedPrefix').'.delete']);
+    $router->post('/replace', ['uses' => 'FilesController@replace', 'as' => config('filetools.router.namedPrefix').'.replace']);
+    $router->post('/update', ['uses' => 'FilesController@update', 'as' => config('filetools.router.namedPrefix').'.update']);
+    $router->post('/reorder', ['uses' => 'FilesController@reorder', 'as' => config('filetools.router.namedPrefix').'.reorder']);
 });
